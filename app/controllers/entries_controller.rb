9a89f7142791da689c672693995db633a4f5286b
SeiyaIwanabe <mystory.i.seiya@gmail.com>
2020-10-14 17:59:39 +0900

    修正

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,10 +1,12 @@
 class EntriesController < ApplicationController
 
   def create
-     # こう記述することで、「current_userに関連したFavoriteクラスの新しいインスタンス」が作成可能。
+    # @event = Event.find(params[:id])
+    # こう記述することで、「current_userに関連したFavoriteクラスの新しいインスタンス」が作成可能。
     # つまり、favorite.user_id = current_user.idが済んだ状態で生成されている。
     # buildはnewと同じ意味で、アソシエーションしながらインスタンスをnewする時に形式的に使われる。
     @entry = current_user.entries.build(event_id: params[:event_id])
+    # @event.update(applicant_id: current_user.id)
     if @entry.save
       redirect_to root_path
     else
@@ -12,9 +14,5 @@ class EntriesController < ApplicationController
     end
   end
 
-  def show_entries
-    @event = Event.find(params[:id])
-    @applicants = Entry.where(event_id: @event.id).all
-  end
   
 end
